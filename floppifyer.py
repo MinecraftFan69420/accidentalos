a29f41c066f5026c8832c8bef8e10d5bef43a4c9
MinecraftFan69420 <gelay232000@yahoo.com>
2026-04-26 17:15:47 +0300

    renamed kernel to boot

--- floppifyer.py
+++ floppifyer.py
@@ -1,10 +1,10 @@
 BOOT_SECTOR_SIZE = 512
 FLOPPY_SIZE = 1474560  # 1.44 MB
 
-KERNEL_PATH = r"kernel.bin"
+BOOT_PATH = r"boot.bin"
 IMG_PATH    = r"accidentalos.img"
 
-with open(KERNEL_PATH, "rb") as f:
+with open(BOOT_PATH, "rb") as f:
     boot_sector = f.read()
 
 if len(boot_sector) != BOOT_SECTOR_SIZE:
