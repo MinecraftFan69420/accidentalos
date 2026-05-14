2b804ea540ac11c0c595b8032d49237b0813facf
MinecraftFan69420 <gelay232000@yahoo.com>
2026-05-14 17:54:14 +0300

    updated the floppifyer

--- floppifyer.py
+++ floppifyer.py
@@ -3,6 +3,7 @@ FLOPPY_SIZE = 1474560  # 1.44 MB
 
 BOOT_PATH = r"boot.bin"
 DEFAULT_HEADER_PATH = r"defaultheader.bin"
+DEFAULT_FILE_TABLE_PATH = r"defaultfiletable.bin"
 KERNEL_PATH = r"kernel.bin"
 IMG_PATH    = r"accidentalos.img"
 
@@ -15,13 +16,17 @@ with open(KERNEL_PATH, "rb") as k:
 with open(DEFAULT_HEADER_PATH, "rb") as h:
     default_header = h.read()
 
+with open(DEFAULT_FILE_TABLE_PATH, "rb") as t:
+    default_file_table = t.read()
+
 if len(boot_sector) != BOOT_SECTOR_SIZE:
     raise ValueError(f"Boot sector must be exactly 512 bytes (got {len(boot_sector)})")
 
 with open(IMG_PATH, "wb") as f:
     f.write(boot_sector)
     f.write(default_header)
-    f.write(b"\x00" * ((32-1)* 512))
+    f.write(default_file_table)
+    f.seek(33 * 512) # move to sector 33
     f.write(kernel)
     remaining = FLOPPY_SIZE - f.tell()
     if remaining > 0:
