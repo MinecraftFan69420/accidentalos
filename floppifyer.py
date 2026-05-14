bc687bbbf2b19e7750de321e49a07ea4463ede8b
MinecraftFan69420 <gelay232000@yahoo.com>
2026-05-14 20:24:12 +0300

    updated binaries again

--- floppifyer.py
+++ floppifyer.py
@@ -6,6 +6,7 @@ DEFAULT_HEADER_PATH = r"defaultheader.bin"
 DEFAULT_FILE_TABLE_PATH = r"defaultfiletable.bin"
 KERNEL_PATH = r"kernel.bin"
 IMG_PATH    = r"accidentalos.img"
+LOREM_IPSUM_TEST_PATH = r"test.bin"
 
 with open(BOOT_PATH, "rb") as b:
     boot_sector = b.read()
@@ -19,6 +20,9 @@ with open(DEFAULT_HEADER_PATH, "rb") as h:
 with open(DEFAULT_FILE_TABLE_PATH, "rb") as t:
     default_file_table = t.read()
 
+with open(LOREM_IPSUM_TEST_PATH, "rb") as l:
+    lorem_ipsum_test = l.read()
+
 if len(boot_sector) != BOOT_SECTOR_SIZE:
     raise ValueError(f"Boot sector must be exactly 512 bytes (got {len(boot_sector)})")
 
@@ -26,8 +30,10 @@ with open(IMG_PATH, "wb") as f:
     f.write(boot_sector)
     f.write(default_header)
     f.write(default_file_table)
-    f.seek(17 * 512) # move to sector 17
+    f.seek(17 * 512) # move to sector 17 (0-based)
     f.write(kernel)
+    f.seek(21 * 512) # move to sector 21
+    f.write(lorem_ipsum_test)
     remaining = FLOPPY_SIZE - f.tell()
     if remaining > 0:
         f.write(b"\x00" * remaining)
